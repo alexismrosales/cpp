763757377f3e7f710e2647f7c478e9ea8f324838
alexismrosales <aleisjorge91@gmail.com>
2024-12-17 23:54:28 -0600

    new problems solved

--- leetcode/Python/algorithms/215.py
+++ leetcode/Python/algorithms/215.py
@@ -5,35 +5,52 @@ import random
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
 
-        def partition(nums, start, end):
-            random_pivoting = random.randint(start, end)
-            swap(nums, end, random_pivoting)  # Move random pivot to the end
+        def merge_sort(nums, left, right):
+            if left < right:
+                mid = (left + right) // 2
+                merge_sort(nums, left , mid)
+                merge_sort(nums, mid + 1, right)
+                merge(nums, left, mid, right)
 
-            pivot = nums[end]  # Use the last element as the pivot
-            i = start
+        def merge(nums, left, mid, right):
+            # Size of arrays to subarray
+            # is mid - left because is needed the range from start to mid
+            n1 = mid - left + 1
+            n2 = right - mid
 
-            # Modify comparison for descending order
-            for j in range(start, end):
-                if nums[j] >= pivot:  # Greater instead of less
-                    swap(nums, i, j)
+            # Creating arrays
+            L = [0] * n1
+            R = [0] * n2
+
+            # Saving numbers in temporal arrays
+            for i in range(n1):
+                L[i] = nums[left + i]
+            for j in range(n2):
+                R[j] = nums[mid + 1 + j]
+
+            i = 0 # Initial index of first subarray
+            j = 0 # Initial index of second subarray
+            k = left #Initial index of merged subarray
+
+            # Merge temp arrays into the original one but in order
+            while i < n1 and j < n2:
+                if L[i] >= R[j]: # If L[i] val is smaller is save in the array else R[j]
+                    nums[k] = L[i]
                     i += 1
-
-            swap(nums, i, end)  # Move pivot to the correct position
-            return i
-
-        def swap(nums, i, j):
-            nums[i], nums[j] = nums[j], nums[i]
-
-        start, end = 0, len(nums) - 1
-        k = k - 1
-        while start <= end:
-            pivot_index = partition(nums, start, end)
-            if pivot_index == k:
-                return nums[pivot_index]
-            elif pivot_index < k:
-                start = pivot_index + 1
-            else:
-                end = pivot_index - 1
-        return 0
+                else:
+                    nums[k] = R[j]
+                    j += 1
+                k += 1
+            
+            # In case there are remaining elements on left or right array just add the rest
+            while i < n1:
+                nums[k] = L[i]
+                i+=1
+                k+=1
+            while j < n2:
+                nums[k] = R[j]
+                j+=1
+                k+=1
+        merge_sort(nums, 0, len(nums)-1)
+        return nums[k-1]
